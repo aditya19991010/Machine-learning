7dded024ff52ba64077bb3fea5f0435cca54cc26
aditya19991010 <aditya19991010@gmail.com>
2025-01-17 17:02:25 +0530

    preparing linear reg model and code cost func

---
+++ lab3/ex2.py
@@ -0,0 +1,22 @@
+#Linear regression
+# - Write a function to compute hypothesis
+# - Write a function to compute the cost
+# - Write a function to compute the derivative
+# - Write update parameters logic in the main function
+
+import numpy as np
+import pandas as pd
+
+X = [[1,1,2],
+     [2,3,4],
+     [3,4,7]]
+y = [2,3,5]
+
+def X_y(df):
+    y = df.iloc[]
+def calc_hx(X):
+
+    # initialize
+    theta=0
+    for i in len(X):
+        hx=theta0
